fc9ada7f825dfe9640358c1e05f3e9d42418bfb0
Alex Fedotov <alxfed@gmail.com>
2026-04-14 08:11:45 -0500

    correction

--- src/thinking_machine/config.py
+++ src/thinking_machine/config.py
@@ -15,9 +15,9 @@ class Config:
     github_name: str                = field(default_factory=lambda: environ.get('GITHUB_NAME', ''))
     github_email: str               = field(default_factory=lambda: environ.get('GITHUB_EMAIL', ''))
     provider_api_key: str           = field(default_factory=lambda: environ.get('PROVIDER_API_KEY', ''))
-    provider: str                   = field(default_factory=lambda: environ.get('PROVIDER', ''))
+    provider: str                   = field(default_factory=lambda: environ.get('PROVIDER', 'OpenAI'))
     machine_organization_name: str  = field(default_factory=lambda: environ.get('MACHINE_ORGANIZATION_NAME', 'machina-ratiocinatrix'))
     private_repo_with_text: str     = field(default_factory=lambda: environ.get('PRIVATE_REPO_WITH_TEXT','thinking_machine'))
     system_prompt_file: str         = field(default_factory=lambda: environ.get('SYSTEM_PROMPT_FILE', 'machina.yaml'))
-    name: str                       = ''
-    instructions: str               = ''
+    name: str                       = field(default_factory=lambda: environ.get('MACHINE-NAME','thinking-machine'))
+    instructions: str               = field(default_factory=lambda: environ.get('MACHINE-DESCRIPTION','The Assistant is Thinking-Machine.'))
